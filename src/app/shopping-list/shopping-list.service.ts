ad166d3bc49e7ccf183553b7aa5cf464d84a8ec4
Dante L'Erario <dantelerario@gmail.com>
2022-09-20 18:03:34 +0200

    appyling angular form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,7 +3,8 @@ import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
-
+    startedEditing = new Subject<number>();
+    
     private ingredients : Ingredient[] = [
         new Ingredient('Bananas', 5),
         new Ingredient('Melons', 2),
@@ -12,6 +13,10 @@ export class ShoppingListService {
 
     getIngredients() {
         return this.ingredients.slice();
+    }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
     }
 
     addIngredient(ingredient: Ingredient) {
@@ -25,5 +30,10 @@ export class ShoppingListService {
         // }
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient) {
+      this.ingredients[index] = newIngredient;  
+      this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
