ea5642811f038390ada9696bf6fadc93078da602
Dante L'Erario <dantelerario@gmail.com>
2022-10-10 15:31:55 +0200

    auth UI, login token, authentication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 import { DataStorageService } from '../shared/data-storage.service';
 
@@ -7,13 +9,19 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
 
   collapsed = true;
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !user ? false : true;
+      // this.isAuthenticated = !!user; //doppia negazione, stessa cosa di sopra
+    });
   }
 
   onSaveData() {
@@ -23,6 +31,9 @@ export class HeaderComponent implements OnInit {
   onFetchData() {
     this.dataStorageService.fetchRecipes()        
     .subscribe();
+  }
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
 }
