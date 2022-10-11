38884c27c63d7b2ea76c5d2c7ca69006e14fad3f
Dante L'Erario <dantelerario@gmail.com>
2022-10-11 17:29:07 +0200

    logout done, autologin done, interceptor done

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, throwError } from "rxjs";
+import { Router } from "@angular/router";
 
 import { User } from "./user.model";
 
@@ -19,7 +20,7 @@ export interface AuthResponseData {
 export class AuthService {
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) {}
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC-oRlJLWbjlylgJNqHXgDGsrz2xTGAMSk',
@@ -43,11 +44,36 @@ export class AuthService {
         }));
     }
 
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
+    }
+
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
 
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -12,32 +12,27 @@ export class DataStorageService {
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
-        return this.http.put('https://ng-cooking-recipe-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
+        return this.http
+        .put('https://ng-cooking-recipe-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
         .subscribe(response => {
             console.log(response);
         });
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1), 
-            exhaustMap(user => {
-            return this.http
-            .get<Recipe[]>('https://ng-cooking-recipe-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-                {
-                    params: new HttpParams().set('auth', user.token)
-                }
-            );
-        }),
-        map(recipes => {
+        return this.http
+        .get<Recipe[]>('https://ng-cooking-recipe-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+        )
+        .pipe(   
+            map(recipes => {
             return recipes.map(recipe => {
                 return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []
                 };
             })
-        }),
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-        })
+            }),
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
         );
-    }
+    }  
 }
